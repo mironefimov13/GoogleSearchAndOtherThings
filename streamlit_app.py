d8d26bd22f37aedab6d79b15095466473f5bf6f7
mironefimov13 <73971589+mironefimov13@users.noreply.github.com>
2024-12-20 16:52:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,26 @@
 import streamlit as st
 from icrawler.builtin import GoogleImageCrawler
 import nltk
+import translators as ts
 from nltk.corpus import wordnet
 
 try:
     from googlesearch import search
+    from PyDictionary import PyDictionary
 except ImportError: 
     print("No module named 'google' found")
 
+dictionary = PyDictionary()
 links = st.text_input("Search Google Links:")
 numlinks = st.slider("Number of Links:", 1, 300)
-submitlinks = st.button("Submit Links")
+submitlinks = st.button("Submit")
 
 if submitlinks:
     for j in search(links, tld="co.in", num=numlinks, stop=numlinks, pause=2):
         st.write(j)
 
 findsynonyms = st.text_input("Find synonyms for: ")
-submits = st.button("Submit")
+submits = st.button("Submit ")
 
 if submits:
     synonyms = []
@@ -29,3 +32,16 @@ if submits:
     print(synonyms)
     for i in range(len(synonyms)):
         st.write(synonyms[i])
+    st.write(dictionary.meaning(findsynonyms))
+
+translate = st.text_input("Translate:")
+language = st.selectbox("Which Language?", ["French", "Spanish"])
+submittrans = st.button("Submit  ")
+
+if submittrans:
+    if language == "French":
+        lan = "fr"
+    elif language == "Spanish":
+        lan = "sp"
+    res = ts.deepl(translate, lan)
+    st.write(res)
